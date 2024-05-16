9bd3a16e21dbb0b88261a336abc555276941d48e
Matthias Koenig <konigmatt@googlemail.com>
2024-05-16 14:41:59 +0200

    retrieving unit information from petab

--- src/petabunit/petabunits.py
+++ src/petabunit/petabunits.py
@@ -1,3 +1,7 @@
+from typing import Dict
+
+import pint
+
 import petab
 from petabunit import EXAMPLES_DIR
 from petabunit.console import console
@@ -17,21 +21,53 @@ class PEtabUnitParser:
 
     def __init__(self, problem: petab.Problem) -> None:
         self.problem = problem
+        self.model_units: Dict[str, str] = {}
+        self.parameter_units: Dict[str, str] = {}
+        self.observable_units: Dict[str, str] = {}
+        self.measurement_units: Dict[str, str] = {}
+        self.condition_units: Dict[str, str] = {}
 
-    @staticmethod
-    def units_for_petab_problem(problem: petab.Problem):
+        self.petab_units()
+
+
+    def petab_units(self):
         """Resolve all units for a given problem."""
 
+        # parameter
+        df = self.problem.parameter_df
+        units = {}
+        if PARAMETER_UNIT_COLUMN in df.columns:
+            for oid, row in df.iterrows():
+                units[oid] = row[PARAMETER_UNIT_COLUMN]
+        self.parameter_units = units
         console.rule("parameters", style="white")
-        parameter_df = problem.parameter_df
+        console.print(df)
+        console.print(f"{self.parameter_units=}")
 
-        # Check the observable dataframe
+        # observable
+        df = self.problem.observable_df
+        units = {}
+        if OBSERVABLE_UNIT_COLUMN in df.columns:
+            for _, row in df.iterrows():
+                units[row["observableFormula"]] = row[OBSERVABLE_UNIT_COLUMN]
+        self.observable_units = units
         console.rule("observables", style="white")
-        console.print(problem.observable_df)
+        console.print(df)
+        console.print(f"{self.observable_units=}")
 
         # Check the measurement dataframe
-        console.rule("measurements", style="white")
-        console.print(problem.measurement_df)
+        df = self.problem.measurement_df
+        units = {}
+        if MEASUREMENT_UNIT_COLUMN in df.columns:
+            for _, row in df.iterrows():
+                # get the observableName
+                problem.observable_df[]
+
+                units[row["observableFormula"]] = row[OBSERVABLE_UNIT_COLUMN]
+        self.observable_units = units
+        console.rule("observables", style="white")
+        console.print(df)
+        console.print(f"{self.observable_units=}")
 
         # check the condition dataframe
         console.rule("conditions", style="white")
@@ -72,7 +108,7 @@ if __name__ == "__main__":
         console.print(f"PEtab errors: {errors_exist}")
 
         petab_uparser = PEtabUnitParser(problem)
-        PEtabUnitParser.print_problem(problem)
+        # PEtabUnitParser.print_problem(problem)
 
 
 
